57ac66e2620ca367e1c4bd1f495bd519c1b63466
CoolyDucks <peguinboy2015@gmail.com>
2026-02-14 14:45:43 +0300

    Shell command do the Desktop You need

---
+++ home.sh
@@ -0,0 +1,17 @@
+mkdir music
+mkdir apps
+mkdir development
+mkdir -p development/Go
+mkdir -p development/C++
+mkdir -p development/C
+mkdir -p development/Python 
+mkdir -p development/Lua
+mkdir -p development/JavaScript 
+mkdir -p development/TypeScript 
+mkdir -p development/Java
+mkdir -p development/Cs
+mkdir pictures
+mkdir notes
+mkdir document
+echo "Your Home done!"
+ls
